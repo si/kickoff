b7fcb9cf740c432111fdbf330f820f42e08fbacb
Simon Jobling <simon.jobling@gmail.com>
2014-01-16 08:38:51 +0000

    Hijacked user login form to warn users authentication isn't integrated yet

--- app/webroot/js/events.js
+++ app/webroot/js/events.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+  $('.navbar-form').bind('submit',function(e){
+    e.preventDefault();
+    alert("Authentication hasn't been built yet.");
+    return false;
+  });
+
+
   /* Auto-populate Event Summary from home and away teams */
   $('#EventHome, #EventAway').keydown(function(e){
     $('#EventSummary').val($('#EventHome').val() + ' v ' + $('#EventAway').val());
