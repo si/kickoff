c3e8dc1a72cbe4a6d126e92d707216b03e99d307
Si Jobling <simon.jobling@gmail.com>
2014-09-22 18:16:18 +0100

    Allow access to static pages when logged out
    Redirect to Calendars index on successful login.

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -3,12 +3,17 @@ App::uses('Controller', 'Controller');
 
 class AppController extends Controller {
 
-  public $helpers = array('Html','Form','Time');
+  	public $helpers = array('Html','Form','Time');
 	public $components = array('Auth', 'Session');
 	public $uses = array('AppUser');
 
 	public function beforeRender() {
 		$this->set('authuser', $this->AppUser->read(null, $this->Auth->user('id')));
+	}
+
+	public function beforeFilter() {
+		$this->Auth->allow('display');
+		$this->Auth->loginRedirect = array('controller'=>'calendars', 'action'=>'index');
 	}
 
 }
