bb78111fc093c331839d6cf89937e744efb4d49e
Si Jobling <simon.jobling@gmail.com>
2015-06-19 07:37:14 +0100

    Fix calendar theme background when editing an event

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -64,8 +64,9 @@ class EventsController extends AppController {
   	$this->set('calendars', $this->Event->Calendar->find('list'));
   	$this->set('teams', $this->Event->HomeTeam->find('list')); 
 
-    if(isset($this->params['named']['calendar'])) {
-      $this->set('calendar', $this->Event->Calendar->findById($this->params['named']['calendar']));
+    if(isset($this->params['named']['calendar']) || $this->data['Event']['calendar_id']!='') {
+      $calendar = ($this->data['Event']['calendar_id']) ? $this->data['Event']['calendar_id'] : $this->params['named']['calendar'];
+      $this->set('calendar', $this->Event->Calendar->findById($calendar));
     }
   	
 	}
