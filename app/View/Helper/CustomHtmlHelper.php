85aee4807fec97d7f60b95f69c85957fdb78fccd
Si Jobling <simon.jobling@gmail.com>
2015-06-26 07:18:15 +0100

    Fix permission check on adminLinks to use Session helper instead of native PHP $_SESSION var. Create new adminPostLink helper for postLinks that need admin privileges.

--- app/View/Helper/CustomHtmlHelper.php
+++ app/View/Helper/CustomHtmlHelper.php
@@ -3,11 +3,19 @@ App::uses('HtmlHelper', 'View/Helper');
 
 class CustomHtmlHelper extends HtmlHelper {
 
-	var $helpers = array('Html');
+	var $helpers = array('Html', 'Form', 'Session');
 
 	public function adminLink($text, $path, $args) {
-		if( isset($_SESSION['Auth']['User']['is_admin']) && $_SESSION['Auth']['User']['is_admin'] ) {
+		if( $this->Session->read('Auth.User.is_admin') ) {
 			return $this->Html->link($text, $path, $args);
 		}
 	}
+
+	public function adminPostLink($text, $path, $args) {
+		if( $this->Session->read('Auth.User.is_admin') ) {
+			return $this->Form->postLink($text, $path, $args);
+		}
+	}
+
+
 }
