0c12d2e522a218526b73c0928aac077762122f2a
Si Jobling <simon.jobling@gmail.com>
2015-07-27 07:28:09 +0100

    Add Competition to Team belongsTo relationship

--- app/Model/Team.php
+++ app/Model/Team.php
@@ -4,7 +4,7 @@ class Team extends AppModel {
   var $name = 'Team';
   var $order = array('Sport.name ASC', 'Team.name ASC');
 
-  var $belongsTo = array('Sport','Theme');
+  var $belongsTo = array('Competition','Sport','Theme');
   var $hasMany  = array(
     'Event' => array(
       'foreignKey' => 'home_team_id',
