7d7828fc9a88e0bb2b5a0bfedf96f1918b54d415
Si Jobling <simon.jobling@gmail.com>
2015-07-30 07:50:46 +0100

    Allow sluggable URLs on sports (eg. /sports/view/football)

--- app/Controller/SportsController.php
+++ app/Controller/SportsController.php
@@ -1,18 +1,27 @@
 <?php
 class SportsController extends AppController {
 
-  var $name = 'Sports';
-  var $helpers = array('Time','Form');
+	var $name = 'Sports';
+	var $helpers = array('Time','Form');
 
-  var $scaffold;
-  
+	var $scaffold;
+
 	function beforeFilter() {
 		parent::beforeFilter();
-        $this->Auth->allow('index', 'view');
+		$this->Auth->allow('index', 'view');
 	}
-	
+
 	function beforeRender() {
-	  parent::beforeRender();
+		parent::beforeRender();
 	}
-    
+
+	function view($id='') {
+	    if(is_numeric($id)) {
+		    $sport = $this->Sport->findById($id);
+	    } else {
+		    $sport = $this->Sport->findBySlug($id);
+		}
+		$this->set('sport', $sport);
+	}
+
 }
