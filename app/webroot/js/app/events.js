646408febfa71891d9297eed7cb94582edc2df8d
Si Jobling <simon.jobling@gmail.com>
2015-11-09 22:35:06 +0000

    Apply webcal protocol to calendar links on supported platforms (iOS and OSX for now)

--- app/webroot/js/app/events.js
+++ app/webroot/js/app/events.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
   /* Auto-populate Event Summary from home and away teams */
   var createEventSummary = function() {
     $('#EventSummary').val($('#EventHome').val() + ' v ' + $('#EventAway').val());
@@ -14,5 +13,43 @@ $(document).ready(function(){
     pick12HourFormat: true,
     pickSeconds: false
   });
+
+
+  var isWebCal = function() {
+
+    var iDevices = [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod',
+      'MacIntel'
+    ];
+    while (iDevices.length) {
+      if (navigator.platform === iDevices.pop()){ return true; }
+    }
+    return false;
+  };
+
+  // Set webcal: protocol on supported systems
+  var setWebCalProtocol = function() {
+    if( isWebCal() ) {
+      var $calLinks = $('a[type="text/calendar"]');
+      $calLinks.each(function(i, el){
+        var href = $(el).attr('href'),
+            domain = window.location.hostname,
+            protocol = 'webcal://';
+        if(href.indexOf(domain) === -1) {
+          href = protocol + domain + href;
+        } else {
+          href = href.replace('http://', protocol);
+        }
+        $(el).attr('href', href);
+      });
+    }
+  };
+
+  setWebCalProtocol();
   
 });
--- app/webroot/js/min/kickoff-v0.1.0.js
+++ app/webroot/js/min/kickoff-v0.1.0.js
@@ -3360,7 +3360,6 @@ u[o]&&(delete u[o],c?delete n[l]:typeof n.removeAttribute!==i?n.removeAttribute(
 })(window.jQuery);
 $(document).ready(function(){
 
-
   /* Auto-populate Event Summary from home and away teams */
   var createEventSummary = function() {
     $('#EventSummary').val($('#EventHome').val() + ' v ' + $('#EventAway').val());
@@ -3374,5 +3373,43 @@ $(document).ready(function(){
     pick12HourFormat: true,
     pickSeconds: false
   });
+
+
+  var isWebCal = function() {
+
+    var iDevices = [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod',
+      'MacIntel'
+    ];
+    while (iDevices.length) {
+      if (navigator.platform === iDevices.pop()){ return true; }
+    }
+    return false;
+  };
+
+  // Set webcal: protocol on supported systems
+  var setWebCalProtocol = function() {
+    if( isWebCal() ) {
+      var $calLinks = $('a[type="text/calendar"]');
+      $calLinks.each(function(i, el){
+        var href = $(el).attr('href'),
+            domain = window.location.hostname,
+            protocol = 'webcal://';
+        if(href.indexOf(domain) === -1) {
+          href = protocol + domain + href;
+        } else {
+          href = href.replace('http://', protocol);
+        }
+        $(el).attr('href', href);
+      });
+    }
+  };
+
+  setWebCalProtocol();
   
 });
