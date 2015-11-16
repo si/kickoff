e7859d3b7d98b49fa09f888fb1295fe23f3a17a8
Si Jobling <simon.jobling@gmail.com>
2015-11-16 19:23:30 +0000

    Add Hello shell for username

--- app/Console/Command/HelloShell.php
+++ app/Console/Command/HelloShell.php
@@ -1,6 +1,13 @@
 <?php
 class HelloShell extends AppShell {
+
+    public $uses = array('User');
+
     public function main() {
         $this->out('Hello world.');
+    }
+    public function show() {
+        $user = $this->User->findByUsername($this->args[0]);
+        $this->out(print_r($user, true));
     }
 }
