2447076bc73b613fd9aca20bac5e5c08febbff66
Si Jobling <simon.jobling@gmail.com>
2015-12-07 18:11:01 +0000

    Fix date time picker on events end field and add functionality to copy start date/time on blur

--- app/webroot/js/min/kickoff-v0.1.0.js
+++ app/webroot/js/min/kickoff-v0.1.0.js
@@ -3363,17 +3363,27 @@ $(document).ready(function(){
   /* Auto-populate Event Summary from home and away teams */
   var createEventSummary = function() {
     $('#EventSummary').val($('#EventHome').val() + ' v ' + $('#EventAway').val());
-  }
+  };
 
   $('#EventHome, #EventAway').on('change', createEventSummary);
   $('#EventHome, #EventAway').on('keyup', createEventSummary);
 
   // Apply date/time picker to event start and end fields
-  $('#EventStart, #EventEnd').datetimepicker({
+  $('#EventStart, #EventEnds').datetimepicker({
     language: 'en',
     pick12HourFormat: true,
     pickSeconds: false
   });
+
+  var updateEventEnd = function() {
+    var end = $('#EventEnds');
+    window.console && console.log(end);
+    if(end.val() === '') {
+      end.val( $('#EventStart').val() );
+    }
+  };
+
+  $('#EventStart').on('blur', updateEventEnd);
 
   // Setup calendar shortcuts
   var addShortcuts = function() {
