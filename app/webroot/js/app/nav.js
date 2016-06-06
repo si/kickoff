4632e74466b5bab824609c575303e3a6824f2261
Si Jobling <simon.jobling@gmail.com>
2016-06-06 07:39:53 +0100

    Initiate Slick on navigation teams

--- app/webroot/js/app/nav.js
+++ app/webroot/js/app/nav.js
@@ -2,4 +2,12 @@ $(function(){
     $('button.toggle-nav').on('click', function() {
         $(this).toggleClass('expand');
     });
+    
+	$(".slick-nav").slick({
+		dots: true,
+		infinite: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: true,
+	});
 });
