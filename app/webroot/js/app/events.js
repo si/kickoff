42cddef6c27adff47d9036d242714a900913dad0
Si Jobling <simon.jobling@gmail.com>
2016-06-14 07:16:59 +0100

    Apply slick carousel to timezone list

--- app/webroot/js/app/events.js
+++ app/webroot/js/app/events.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
+  /* Apply slick carousel to timezone list */
+  $("#timezones .continents").slick({
+		dots: true,
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		draggable: true,
+	});
+
   /* Auto-populate Event Summary from home and away teams */
   var createEventSummary = function() {
     $('#EventSummary').val($('#EventHome').val() + ' v ' + $('#EventAway').val());
