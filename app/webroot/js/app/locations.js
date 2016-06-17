8d97ad159bfd8b110e6ce2fb1930cd2365eed394
Si Jobling <simon.jobling@gmail.com>
2016-06-17 07:31:47 +0100

    First draft of previewing location coordinates on Google Maps

---
+++ app/webroot/js/app/locations.js
@@ -0,0 +1,23 @@
+// Auto-update location preview based on lat/long settings
+$(function(){
+    var getCoords = function() {
+        console.log('Get coordinates');
+        return $('#LocationLat').val() + ',' + $('#LocationLong').val();
+    };
+
+    var setPreview = function() {
+        console.log('Set preview');
+        var center = getCoords(),
+            googleMapUrl = '//maps.googleapis.com/maps/api/staticmap?center=' + center 
+                + '&amp;size=800x600&amp;style=element:labels|visibility:off&amp;style=element:geometry.stroke|visibility:off&amp;style=feature:landscape|element:geometry|saturation:-100&amp;style=feature:water|saturation:-100|invert_lightness:true&amp;key=AIzaSyBMISecHzJR_Mie1nlsQWpQkv-E6B7ZFno';
+        $('.location-preview')
+            .css('background-image', 'url(' + googleMapUrl + ')')
+            .text(center);
+    };
+
+    var setListeners = function() {
+        console.log('Set listeners');
+        $('#LocationLat, #LocationLong').on('change', setPreview);
+    }();
+
+});
