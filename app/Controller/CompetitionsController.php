1b0555685cb9002566d502f8e1021fd3c31956f3
Si Jobling <simon.jobling@gmail.com>
2016-06-27 07:42:38 +0100

    Set default competition month to next available game.

--- app/Controller/CompetitionsController.php
+++ app/Controller/CompetitionsController.php
@@ -75,7 +75,18 @@ class CompetitionsController extends AppController {
 			if(isset($this->params['named']['month'])) {
 				$start = strtotime($this->params['named']['month']."-01 00:00:00");
 			} else {
-				$start = strtotime(date('Y-m')."-01 00:00:00");
+				$next = $this->Competition->Event->find('first', array(
+					'fields'=>array(
+						'start'
+					), 
+					'conditions'=>$future_params['conditions'], 
+					'order'=>array('start ASC')
+				));
+				if(count($next)>0) {
+					$start = strtotime($next['Event']['start']);
+				} else {
+					$start = strtotime(date('Y-m')."-01 00:00:00");
+				}
 			} 
 
 		    // Set end date to passed parameter if defined, next month if not
